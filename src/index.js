8404f91983b5a42ade0defb5192bff3106e5e72e
suntax777 <43720142+suntax777@users.noreply.github.com>
2021-12-29 19:04:17 +0900

    スプレッド構文

--- src/index.js
+++ src/index.js
@@ -63,3 +63,27 @@
 // const [name, age] = myProfile;
 // const message4 = `名前は${name}です。年齢は${age}です。`;
 // console.log(message4);
+
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん！`);
+// sayHello();
+
+// const arr1 = [1, 2];
+// // console.log(arr1)
+// // console.log(...arr1)
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(arr3)
+
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// console.log(arr6);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
