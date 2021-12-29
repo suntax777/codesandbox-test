be79df5852ce0f9fabd11af7338be681e3ab4a4d
suntax777 <43720142+suntax777@users.noreply.github.com>
2021-12-29 18:27:38 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -1,10 +1,65 @@
-import "./styles.css";
+// var val1 = "var変数";
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// const val4 = {
+//   name: "hiro",
+//   age: 28
+// };
+// console.log(val4);
+
+// val4.name = "koba";
+// val4.address = "Gifu";
+// console.log(val4);
+
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+// const name = "Hiro";
+// const age = 41;
+
+// //私の名前はHiroです。年齢は41です。
+
+// const message1 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message1);
+
+// const func2 = (str) => {
+//   return str
+// }
+// console.log(func2("hirohiro"))
+
+// const func3 = (num1, num2) => {
+//   return num1 + num2
+// }
+// console.log(func3(4,87));
+
+// const myProfile = {
+//   name: "hiro",
+//   age: 41
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+// const myProfile = ["hiroshi", 41];
+
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`;
+// console.log(message3);
+
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}です。`;
+// console.log(message4);
